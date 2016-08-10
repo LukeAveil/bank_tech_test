d3a8750f97d233924d19b346b6bb383b049c5913
Luke <lukeaveil@yahoo.com>
2016-08-10 13:57:43 +0100

    log transaction test

--- lib/transactionlog.rb
+++ lib/transactionlog.rb
@@ -1,9 +1,17 @@
+require_relative 'transaction'
+
 class TransactionLog
 
   attr_reader :transactions
 
-  def initialize
+  def initialize(transaction_class = Transaction)
+    @transaction_class = transaction_class
     @transactions = []
+  end
+
+  def log_transaction
+    @transaction = @transaction_class.new
+    @transactions << @transaction
   end
 
 end
--- spec/transactionlog_spec.rb
+++ spec/transactionlog_spec.rb
@@ -1,12 +1,23 @@
 require 'transactionlog'
 
+
 describe TransactionLog do
 
-  subject(:transactionlog) { described_class.new }
+  subject(:transactionlog) { described_class.new(transaction_class) }
+
+  let(:transaction_class) { double :transaction_class, new: transaction }
+  let(:transaction) { double :transaction }
 
   describe "upon initialization" do
     it "has no transaction records" do
       expect(transactionlog.transactions).to eq []
+    end
+  end
+
+  describe "#log_transaction" do
+    it "logs a record of a transaction" do
+      transactionlog.log_transaction
+      expect(transactionlog.transactions).to include transaction
     end
   end
 
